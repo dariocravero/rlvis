c728bafd3d7858e610bc67d07ece07a3815fa49c
Darío Javier Cravero <dario@qinnova.com.ar>
2013-04-22 15:04:21 -0300

    first commit

---
+++ panels.js
@@ -0,0 +1,16 @@
+!function($) {
+  'use strict';
+
+  var main = $('#main');
+
+  // $('.main-left .main-element-bar').on('click', function() {
+  //   if (main.is('.main-double-left-panel')) {
+  //     main.removeClass('main-double-left-panel');
+  //   // } else if (main.is('.main-zero-left-panel')) {
+  //   //   main.removeClass('.main-zero-left-panel');
+  //   // } else {
+  //   } else {
+  //     main.addClass('main-zero-left-panel');
+  //   }
+  // });
+}($);
